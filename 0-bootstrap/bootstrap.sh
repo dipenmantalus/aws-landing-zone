69bd45894318cab0eadc8acd211244fbfded27d4
Dipen Parekh <dipen.parekh@mantalus.com>
2026-01-28 01:33:35 +1100

    Config changes for a new account

--- 0-bootstrap/bootstrap.sh
+++ 0-bootstrap/bootstrap.sh
@@ -2,14 +2,18 @@
 
 # Create S3 bucket and dynamodb table in master account for terraform
 terraform -chdir=tf-prerequisites init
+echo "Initialized terraform for tf-prerequisites"
 terraform -chdir=tf-prerequisites apply --auto-approve
 
 # Apply terraform to the bootstap directory and fetch Account Id of newly created account in a variable
 terraform -chdir=bootstrap init -backend-config=backend.conf
-ORG_ID=$(aws organizations describe-organization | jq .Organization.Id)
+ORG_ID=$(aws organizations describe-organization | jq -r .Organization.Id)
+echo "Organization ID is: $ORG_ID"
 terraform -chdir=bootstrap import aws_organizations_organization.org $ORG_ID 
 terraform -chdir=bootstrap apply --auto-approve
+echo "Bootstrap terraform applied successfully"
 
 # Assume role inside newly created account and apply terraform to create CICD stack
 terraform -chdir=cicd init -backend-config=backend.conf
 terraform -chdir=cicd apply --auto-approve
+echo "CICD terraform applied successfully"
