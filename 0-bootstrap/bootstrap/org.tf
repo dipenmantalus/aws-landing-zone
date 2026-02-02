9293da2998e39ebe12955a85e96ad59740b24b57
Dipen Parekh <dipen.parekh@mantalus.com>
2026-02-02 11:42:57 +1100

    Changes for mantalus org

--- 0-bootstrap/bootstrap/org.tf
+++ 0-bootstrap/bootstrap/org.tf
@@ -2,6 +2,6 @@ resource "aws_organizations_organization" "org" {
   feature_set = "ALL"
   aws_service_access_principals = ["sso.amazonaws.com", "config.amazonaws.com", "malware-protection.guardduty.amazonaws.com",
     "guardduty.amazonaws.com", "config-multiaccountsetup.amazonaws.com", "fms.amazonaws.com",
-  "cloudtrail.amazonaws.com", "detective.amazonaws.com", "ram.amazonaws.com", "securityhub.amazonaws.com", "macie.amazonaws.com", ]
+  "cloudtrail.amazonaws.com", "detective.amazonaws.com", "ram.amazonaws.com", "securityhub.amazonaws.com", "macie.amazonaws.com", "access-analyzer.amazonaws.com", ]
   enabled_policy_types = ["SERVICE_CONTROL_POLICY"]
 }
